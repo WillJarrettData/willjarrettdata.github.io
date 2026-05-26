6d26fc1418c89f5854f1b29206b28d549a2b8d49
Will Jarrett <william.d.jarrett@gmail.com>
2026-05-26 14:00:00 +0100

    update bio

--- assets/partials.js
+++ assets/partials.js
@@ -7,7 +7,7 @@ document.getElementById('site-header').innerHTML = `
                     <h2 class="hidden-title">About</h2>
                     <p class="hidden-about-text">I graduated from <strong>Oxford University</strong> with First Class Honours in English in 2017. For the next few years, I worked as an editor on the <strong>Oxford University Press</strong> education website Oxford Owl.</p>
                     <p class="hidden-about-text">In 2020, I moved to New York to pursue a master's degree in Data Journalism at <strong>Columbia University</strong>. From 2021 to 2024, I was a data reporter at <strong>Mission Local</strong> in San Francisco.</p>
-                    <p class="hidden-about-text">I moved back to London and joined <strong>Tortoise Media</strong> as a developer in 2024. Now, I am now working for the <strong>Associated Press</strong> as an Interactive Artist.</p>
+                    <p class="hidden-about-text">I moved back to London and joined <strong>Tortoise Media</strong> as a developer in 2024. I am now working for the <strong>Associated Press</strong> as an Interactive Artist.</p>
                 </div>
                 <div class="hidden-contact">
                     <h2 class="hidden-title">Contact</h2>
